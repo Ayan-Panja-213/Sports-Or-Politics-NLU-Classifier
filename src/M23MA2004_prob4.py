94f4c9e7b2a59887310c3d7beb8188cb78348e92
Ayan Panja <ayanpanjareeju@gmail.com>
2026-02-12 21:54:01 +0530

    Update M23MA2004_prob4.py

--- src/M23MA2004_prob4.py
+++ src/M23MA2004_prob4.py
@@ -6,25 +6,30 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import AdaBoostClassifier,GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
 
+# FUNCTION for DATA PROCESSING
 def data_process():
-    data=pd.read_csv("data/sports-or-politics.csv")
+    data=pd.read_csv("sports-or-politics.csv")
     print("columns exists in data set : ",data.columns.tolist())
+    
+    # NORMALIZE COLUMN NAMES
     data.columns=[col.strip().lower().replace(" ","_") for col in data.columns]
     
-    data=data[data["class_index"].isin([1,2])]
+    # NUMERIC to CLASS NAME MAPPING
     data["label"]=data["class_index"].map({1:"politics",2:"sports"})
     
+    # COMBINING TITLE & DESCRIPTION INTO SINGLE TEXT
     data["full_text"]=(data["title"].astype(str)+" "+data["description"].astype(str))
     texts=data["full_text"]
     labels=data["label"]
     return texts,labels
 
+# FUNCTION for EVALUATING in DIFFERENT ML models                                                   MODELS
 def models_4_clsif(X_train,X_test,Y_train,Y_test,feature_name):
     models={
-        "Naive Bayes": MultinomialNB(),
-        "Logistic Regression": LogisticRegression(max_iter=999),
-        "AdaBoost": AdaBoostClassifier(n_estimators=111,random_state=42),
-        "Gradient Boosting": GradientBoostingClassifier(random_state=42)
+        "Naive Bayes": MultinomialNB(),                                                          # Naive Bayes
+        "Logistic Regression": LogisticRegression(max_iter=999),                                 # Logistic Regression
+        "AdaBoost": AdaBoostClassifier(n_estimators=111,random_state=42),                        # AdaBoost
+        "Gradient Boosting": GradientBoostingClassifier(random_state=42)                         # Gradient Boosting
     }
     print("\n    ")
     print(f"Feature Representation: {feature_name}")
@@ -35,22 +40,26 @@ def models_4_clsif(X_train,X_test,Y_train,Y_test,feature_name):
         acc=accuracy_score(Y_test,pred)
         print(f"{name:<20} Accuracy: {acc:.4f}")
         
+# FUNCTION for FEATURE EXTRACTION & EVALUATION        
 def clsifr():
     texts,labels=data_process()
     
-    x_train,x_test,y_train,y_test=train_test_split(texts,labels,test_size=0.1,random_state=42)
+    # TRAIN & TEST SPLIT 75% TRAIN 25% TEST
+    x_train,x_test,y_train,y_test=train_test_split(texts,labels,test_size=0.25,random_state=42)
     
+    #                   TF-IDF
     tf_idf=TfidfVectorizer(stop_words="english")
     x_train_tfidf=tf_idf.fit_transform(x_train)
-    x_test_tfidf = tf_idf.transform(x_test)
-    models_4_clsif(x_train_tfidf, x_test_tfidf, y_train, y_test, "TF-IDF")
-
+    x_test_tdidf=tf_idf.transform(x_test)
+    models_4_clsif(x_train_tfidf,x_test_tdidf,y_train,y_test,"TF-IDF")
     
+    #                  n-grams(1,2)
     ngram=TfidfVectorizer(stop_words="english",ngram_range=(1,2))
     x_train_ngram=ngram.fit_transform(x_train)
     x_test_ngram=ngram.transform(x_test)
     models_4_clsif(x_train_ngram, x_test_ngram, y_train, y_test, "n-grams (1,2)")
     
+    #                 Bag of Words 
     bag=CountVectorizer(stop_words="english")
     x_train_bag=bag.fit_transform(x_train)
     x_test_bag=bag.transform(x_test)
